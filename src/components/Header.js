52e8a3739738f1ee1f7a78a6128662d91aa64810
April Larsen <hunterapril94@gmail.com>
2022-02-03 21:12:14 -0800

    mobile nav working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 export default function Header() {
+  const [isActive, setActive] = useState(false)
+  function handleClick() {
+    setActive(!isActive)
+  }
   return(
     <div className="header">
-      <Link to='/'><h2>Home</h2></Link>
-      <Link to='/About'><h2>About Me</h2></Link>
-      <Link to='/Research'><h2>Research</h2></Link>
-      <Link to='CV'><h2>CV</h2></Link>
-      <Link to='Contact'><h2>Contact</h2></Link>
+      <button className="hamburger" onClick={handleClick}>
+      <img src="https://img.icons8.com/ios/50/ffffff/menu--v1.png" />
+      </button>
+      <div className={isActive ? "hidden menu-items" : "menu-items"}>
+        <Link to='/'><h2>Home</h2></Link>
+        <Link to='/About'><h2>About Me</h2></Link>
+        <Link to='/Research'><h2>Research</h2></Link>
+        <Link to='CV'><h2>CV</h2></Link>
+        <Link to='Contact'><h2>Contact</h2></Link>
+      </div>
     </div>
   )
 }
